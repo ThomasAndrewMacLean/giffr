75ca682b0246f5f8e636f35732208cc99623879e
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-08-31 11:10:39 +0200

    add some linting 🎀

--- src/reddit-pwa-app.js
+++ src/reddit-pwa-app.js
@@ -1,14 +1,13 @@
 import { LitElement, html } from 'lit-element';
 
 class RedditPwaApp extends LitElement {
+    // ...
 
-  // ...
-
-  render() {
-    return html`
-      <h1>Hello world!</h1>
-    `;
-  }
+    render() {
+        return html`
+            <h1>Hello world!</h1>
+        `;
+    }
 }
 
 customElements.define('reddit-pwa-app', RedditPwaApp);
