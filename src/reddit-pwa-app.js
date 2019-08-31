b49d24db472581938c656c35e268b433cb2d825d
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-08-31 10:58:26 +0200

    init 🚀

---
+++ src/reddit-pwa-app.js
@@ -0,0 +1,14 @@
+import { LitElement, html } from 'lit-element';
+
+class RedditPwaApp extends LitElement {
+
+  // ...
+
+  render() {
+    return html`
+      <h1>Hello world!</h1>
+    `;
+  }
+}
+
+customElements.define('reddit-pwa-app', RedditPwaApp);
