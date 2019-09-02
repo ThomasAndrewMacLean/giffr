b3853c3f2bc1724d3aedd7841e77be52a9267aa3
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2019-09-03 00:16:11 +0200

    stylllez

--- src/reddit-pwa-app.js
+++ src/reddit-pwa-app.js
@@ -1,11 +1,37 @@
-import { LitElement, html } from 'lit-element';
-
+import { LitElement, html, css } from 'lit-element';
+import { c } from './constants.js';
+console.log(c);
 class RedditPwaApp extends LitElement {
     // ...
 
+    static get styles() {
+        return css`
+            :host {
+                background: linear-gradient(45deg, #d62baa, #ffcc00);
+                display: inline-block;
+                height: calc(100vh);
+                width: 100%;
+            }
+            label {
+                background: #3f568e73;
+                border: 3px solid white;
+                padding: 1rem;
+                cursor: pointer;
+            }
+            input {
+                display: none;
+            }
+        `;
+    }
+
     render() {
         return html`
-            <h1>Hello world!</h1>
+            <main>
+                <h1>Giffr!</h1>
+                <canvas></canvas>
+                <label for="fileInput">upload pictures</label>
+                <input type="file" id="fileInput" />
+            </main>
         `;
     }
 }
